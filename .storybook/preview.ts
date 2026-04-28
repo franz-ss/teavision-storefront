c9f9a7079a0b38557227120fe4700b694d7a0117
Franz Evangelista <franz@switchsupply.com>
2026-04-28 13:48:06 +0800

    chore: configure Storybook preview with brand background and globals

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,22 @@
 import type { Preview } from '@storybook/nextjs-vite'
+import '../app/globals.css'
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'brand',
+      values: [
+        { name: 'brand', value: '#F5F0E8' },
+        { name: 'white', value: '#ffffff' },
+      ],
+    },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
