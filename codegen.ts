162e43cb161903c4f10a76f3c4dc8ae9a4f364df
Franz Evangelista <franz@switchsupply.com>
2026-04-28 12:10:59 +0800

    chore: add GraphQL Codegen config (runs when credentials available)

---
+++ codegen.ts
@@ -0,0 +1,20 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  schema: {
+    [`https://${process.env.SHOPIFY_STORE_DOMAIN}/api/2025-01/graphql.json`]: {
+      headers: {
+        'X-Shopify-Storefront-Access-Token':
+          process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN ?? '',
+      },
+    },
+  },
+  documents: ['lib/shopify/queries/**/*.graphql'],
+  generates: {
+    'lib/shopify/types/generated.ts': {
+      preset: 'client',
+    },
+  },
+}
+
+export default config
