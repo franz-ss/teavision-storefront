d1b762f7519a66d5f1ab3741f65c821bc80d5312
Franz Evangelista <franz@switchsupply.com>
2026-05-01 00:17:28 +0800

    chore: exclude .worktrees from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,8 @@ const eslintConfig = defineConfig([
     'out/**',
     'build/**',
     'next-env.d.ts',
+    // Git worktrees
+    '.worktrees/**',
   ]),
   ...storybook.configs['flat/recommended'],
 
